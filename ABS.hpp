55a4a76df28270c4d58b391f4680c8f8d5a85521
leoG06 <leozao.martinez@gmail.com>
2025-11-09 05:23:54 -0500

    ABS 5

--- ABS.hpp
+++ ABS.hpp
@@ -108,7 +108,8 @@ public:
         curr_size_--;
         if(curr_size_ > 1 && curr_size_<=(capacity_/4)){
         int newCap;
-        newCap = capacity_ / 2;
+        if(capacity_==2){newCap=1;}
+        else{newCap = capacity_ / 2;}
         T* newArr = new T[newCap];
         for(int i =0; i<curr_size_;i++){
             newArr[i] = array_[i];

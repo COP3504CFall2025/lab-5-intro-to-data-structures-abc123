395068681532c1c8aab61179f9bc57386ea40e21
leoG06 <leozao.martinez@gmail.com>
2025-11-11 16:54:42 -0500

    LLDQ 1

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -15,22 +15,44 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ(){
+        list = LinkedList<T>();
+    }
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override{
+         list.addHead(item);
+    }
+    void pushBack(const T& item) override{
+        list.addTail(item);
+    }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override{
+        if(list.getCount()==0){throw std::runtime_error("empty popFront");}
+        T temp = list.getHead()->data;
+        list.removeHead();
+        return temp;
+    }
+    T popBack() override{
+        if(list.getCount()==0){throw std::runtime_error("empty popBack");}
+        T temp = list.getTail()->data;
+        list.removeTail();
+        return temp;
+    }
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override{
+        return list.getHead()->data;
+    }
+    const T& back() const override{
+        return list.getTail()->data;
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
 
 

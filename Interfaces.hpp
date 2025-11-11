3cfb501793b30cbb29d396136efb8669e206d019
leoG06 <leozao.martinez@gmail.com>
2025-11-11 17:31:00 -0500

    LLDQ 1

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -28,13 +28,13 @@ class QueueInterface {
 
 template <typename T>
 class DequeInterface {
-    virtual void pushFront(const T& item)=0;
-    virtual void pushBack(const T& item)=0;
-    virtual T popFront()=0;
-    virtual T popBack()=0;
-    virtual const T& front() const=0;
-    virtual const T& back() const=0;
-    virtual std::size_t getSize() const noexcept=0;
+    virtual void pushFront(const T& item);
+    virtual void pushBack(const T& item);
+    virtual T popFront();
+    virtual T popBack();
+    virtual const T& front() const;
+    virtual const T& back() const;
+    virtual std::size_t getSize() const noexcept;
     virtual ~DequeInterface() = default;
 };
 

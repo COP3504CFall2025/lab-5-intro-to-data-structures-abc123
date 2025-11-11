7a449a5c25d0911efb45b468b2bf248af7fca994
leoG06 <leozao.martinez@gmail.com>
2025-11-11 15:25:44 -0500

    LLS 2+ LLQ 1

--- LLQ.hpp
+++ LLQ.hpp
@@ -11,18 +11,31 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLQ();
+    LLQ(){
+        list = LinkedList<T>();
+    }
 
     // Insertion
-    void enqueue(const T& item) override;
+    void enqueue(const T& item) override{
+        list.addTail(item);
+    }
 
     // Deletion
-    T dequeue() override;
+    T dequeue() override{
+        if(list.getCount()==0){throw std::runtime_error("empty pop");}
+        T temp = list.getHead()->data;
+        list.removeHead();
+        return temp;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override{
+        return list.getHead();
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 
 };

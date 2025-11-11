085dedc46e27c04ad7b486a58a9e49c6b45912b6
leoG06 <leozao.martinez@gmail.com>
2025-11-11 15:21:22 -0500

    LLS 1

--- LLS.hpp
+++ LLS.hpp
@@ -11,17 +11,32 @@ private:
     LinkedList<T> list;
 public:
     // Constructor
-    LLS();
+    LLS(){
+        list = LinkedList<T>();
+    }
 
     // Insertion
-    void push(const T& item) override;
+    void push(const T& item) override{
+        list.addTail(item);
+    }
 
     // Deletion
-    T pop() override;
+    T pop() override{
+        if(list.getCount()==0){throw std::runtime_error("empty pop");}
+        T temp = list.getTail()->data;
+        list.removeTail();
+        return temp;
+    }
 
     // Access
-    T peek() const override;
+    T peek() const override{
+        if(list.getCount()==0){throw std::runtime_error("empty peek");}
+        T temp = list.getTail()->data;
+        return temp;
+    }
 
     //Getters
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override{
+        return list.getCount();
+    }
 };
